de32b07c5f909508d5110fd885e84c1cea8466c8
kmtszky <kmtszk711@gmail.com>
2021-03-13 09:03:19 +0000

    [Add] 4-10: User - Book間アソシエーション設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
+  has_many :books, dependent: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
