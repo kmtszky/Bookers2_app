39bbdff1a598db853c713ba8f20b413c54dee079
kmtszky <kmtszk711@gmail.com>
2021-03-13 10:28:06 +0000

    [update] 4-10: routing, 各controller action定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,18 @@ class BooksController < ApplicationController
   def index
   end
 
+  def create
+  end
+
   def edit
   end
 
   def show
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,18 @@ class UsersController < ApplicationController
   def index
   end
 
+  def create
+  end
+
   def edit
   end
 
   def show
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
