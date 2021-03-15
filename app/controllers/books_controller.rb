3f15b08cfc0b970c4dfd19b6fb0274a5eef4883b
kmtszky <kmtszk711@gmail.com>
2021-03-15 18:01:47 +0000

    [fix] 4-10:error check

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,8 +8,14 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @book = Book.new
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def show
@@ -17,14 +23,14 @@ class BooksController < ApplicationController
     @user = @book.user
   end
 
-  def edit
-    @book = Book.find(params[:id])
-  end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
+    else
+      render :edit
+    end
   end
 
   def destroy
