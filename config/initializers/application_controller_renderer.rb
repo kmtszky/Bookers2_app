1b5a1d6a96b477f231314d09404a26d2b192ef50
kmtszky <kmtszk711@gmail.com>
2021-03-15 14:18:33 +0000

    [udpate] 4-10: views/users/show, editページ編集

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '156377ab33699d7c29fc631fd1cedb8c3fc1cc1238a571fc7b91ab878f58476152c8ab81c65b19340ea1bc8d366db2580c6d99ee6b7b93a92864ad03e2e829ac'
