62ad2a27e8580856fe12374aa30010ff44374205
kmtszky <kmtszk711@gmail.com>
2021-03-16 16:48:45 +0000

    [update] 4-10: codeをよりスリムに修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user.id), notice: 'You have updated user successfully.'
     else
