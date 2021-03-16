fa3996cc63fbe5b20f53a3682211100e44f35d89
kmtszky <kmtszk711@gmail.com>
2021-03-16 08:59:22 +0000

    [fix] 4-10: book_registration機能：users/index, show解決

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,8 @@
 class BooksController < ApplicationController
+  before_action :new_book_registration, only: [:show, :index]
+  before_action :find_user, only: [:show, :destroy]
+
   def index
-    @book = Book.new
     @books = Book.all
     @user = current_user
   end
@@ -9,26 +11,27 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@book.id), notice: 'You have created book successfully.'
     else
-      @book = Book.new
       @books = Book.all
-      @user = current_user
       render :index
     end
   end
 
   def show
-    @book = Book.find(params[:id])
     @user = @book.user
   end
 
   def edit
     @book = Book.find(params[:id])
+    if current_user.id = @book.user_id
+      render :edit
+    else
+      render :show
+    end
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
     else
@@ -37,15 +40,21 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
 
   private
 
+  def new_book_registration
+    @book = Book.new
+  end
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
 
+  def find_user
+    @book = Book.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :new_book_registration, only: [:index, :show]
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -24,6 +26,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def new_book_registration
+    @book = Book.new
+  end
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
